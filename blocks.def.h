3a639ffecae3e0ecbb939914e1362970ce7e01f5
Karol Peek <peek.karol@gmail.com>
2023-04-30 15:22:23 +0200

    clock

--- blocks.def.h
+++ blocks.def.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Pamięć/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %H:%M:%S%p'",					1,		0},
+	{"🕓", "date '+%b %d (%a) %H:%M:%S%p'",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
