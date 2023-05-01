1b49e081e97e95c01e24123a189ada0400da5728
Karol Peek <peek.karol@gmail.com>
2023-05-01 19:24:20 +0200

    cpu temp

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Pamięć/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"🧠 ", "free -h | awk '/^Pamięć/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"🕓", "date '+%b %d (%a) %H:%M:%S%p'",					1,		0},
+	{"🌡 ", "dwmb-cpu-temp",					1,		0},
+
+	{"🕓 ", "date '+%b %d (%a) %H:%M:%S%p'",					1,		0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ⟪ ";
 static unsigned int delimLen = 5;
